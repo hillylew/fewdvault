56ee62f5653a4a7235004e7adc80aa559ed07e56
Hillary Lewandowski <hlewan@umich.edu>
2020-05-29 20:48:46 -0400

    Trying a new color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   theme: {
+    fontFamily: {
+      'sans': ['Helvetica', 'Arial', 'sans-serif'],
+    },
     extend: {
       colors: {
         "woodsmoke": "#0e1111",
@@ -8,10 +11,13 @@ module.exports = {
         "limed-spruce": "#3b444b",
         "silver": "#cccccc",
         "red": "#ff0000",
+        "pink": "#F6807E",
         "chartreuse-yellow": "#e3ff00",
         "malachite": "#2af33e",
         "cyan": "#00ffec",
         "purple-pizzazz": "#ff00db",
+        "light-blue": "#4da6ff",
+        "white": "#ffffff"
       }
     }
   },
