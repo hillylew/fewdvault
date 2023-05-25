0fdb415a9c2ba06052da7ca6b125495f0a983008
Hillary Lewandowski <hlewan@umich.edu>
2023-05-25 14:21:09 -0400

    Update eleventy and feed so we can export our content

--- src/site/_includes/layouts/feed.11ty.js
+++ src/site/_includes/layouts/feed.11ty.js
@@ -1,8 +1,6 @@
 module.exports = data => {
   // Filter out an data without a URL.
-  const itemsWithUrl = data.collections.all.filter(item => {
-    return item.url;
-  });
+  const itemsWithUrl = data.collections.recipesAscending;
 
   // Return the proper structure for our search.
   const content = itemsWithUrl.map(item => {
@@ -10,7 +8,20 @@ module.exports = data => {
       title: item.data.title,
       body: item.templateContent,
       text: item.data.text,
-      url: item.url
+      url: item.url,
+      uuid: item.data.uuid,
+      source: item.data.source,
+      source_url: item.data.source_url,
+      yield: item.data.yield,
+      active_time: item.data.active_time,
+      total_time: item.data.total_time,
+      image: item.data.image,
+      favorite: item.data.favorite,
+      tags: item.data.tags,
+      ingredients: item.data.ingredients,
+      instructions: item.data.instructions,
+      notes: item.data.notes,
+      _type: 'recipe'
     };
   });
 
