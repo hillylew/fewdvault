b50a4f8c99276becf1fdf9ccf2ac88087d7237f3
Hillary Lewandowski <hlewan@umich.edu>
2026-06-29 14:30:46 -0400

    fix: troubleshoot github studio builds

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -1,8 +1,13 @@
 import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
+  deployment: {
+    appId: process.env.SANITY_STUDIO_APP_ID,
+  },
+
   api: {
-    projectId: 'at91z4ai',
-    dataset: 'production'
-  }
+    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_DATASET,
+  },
 });
+
